186cc02951f67817399bc57e1e29605a2ccb886b
Aiden-bot220 <aidenye65@gmail.com>
2025-06-18 10:54:57 -0700

    First commit

---
+++ lab2.py
@@ -0,0 +1,21 @@
+# Lab 2: Python Profiles 
+
+## Step 1
+name = "Aiden Ye"
+print(name)
+
+## Step 2
+nickname = "Aids"
+
+## Step 3 
+age = 16.0
+
+## Step 4
+used_python = False
+
+# Step 5
+hobbies = ["games", "eating", "sleeping", "volleyball"]
+
+## Step 6
+favorite_things = {"movie": "How to Train your Dragons", 'food': "rice", 'animal': "dog", 'sport': "volleyball"}
+
