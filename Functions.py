78cdb371d42c363591ed5ed079f6e70b62609b09
Aiden-bot220 <aidenye65@gmail.com>
2025-07-08 14:40:27 -0700

    updated function assignment

--- Functions.py
+++ Functions.py
@@ -7,11 +7,24 @@ menu = {'Pizza': 2.99,
         'Ice cream': 1.49,
          'Churro': 0.79,
         'Soda': 0.89}
-def display_menu():
-    for item, price in menu.items():
-        print(f"{item}: {price:.2f}")
 
-display_menu()
+category = {"mains":['Burger', 'Hot dog', 'Pizza'],
+            "sides":['Churro', 'Cheese', 'Ice cream'],
+            "drink":['Soda']}
+
+def display_menu(menu, category):
+    categorized_menu = {
+        "mains":{},
+        "sides":{},
+        "drink":{}
+    }
+    for category, items in category.items():
+        for item in items:
+            categorized_menu[category][item] = menu[item]
+    return categorized_menu
+organized_menu = display_menu(menu, category)
+print(organized_menu)
+
 def total_price(item1,item2):
     
     
@@ -21,9 +34,13 @@ print(total_price('Soda','Burger'))
 def price_difference(item1,item2):
     return abs(menu[item1]- menu[item2])
 print(price_difference('Hot dog', 'Ice cream'))
-def price_inflation(item,multiplier):
-    return (menu[item]*multiplier)
-print(price_inflation('Churro',2))
-def price_deflation (item,multiplier):
-    return (menu[item]*multiplier)
-print(price_deflation('Churro',0.5))
+def price_inflation(item,menu,multiplier):
+    menu[item] = menu[item] * multiplier
+    return menu
+updated_menu = price_inflation('Soda', menu, 2)
+print(updated_menu)
+def price_deflation (item,menu,multiplier):
+    menu[item] = menu[item] * multiplier
+    return menu
+updated_menu2 = price_deflation('Cheese', menu, .1)
+print(updated_menu2)
